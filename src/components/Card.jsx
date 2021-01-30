adadf7bbb8cd94b91c377cf223c452313564a572
Muhammad Uddin <fahad.uddin@mail.utoronto.ca>
2021-01-30 18:32:11 -0500

    add Card component to render the projects dynamically

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./card.css";
 
-export default function Card (props){
-    return(
-        
-    );
+export default function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src="../images/img-1.jpg" alt="hello" />
+      </div>
+      <div className="bottom">
+        <p className="description">{props.description}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../App.css';
+
+import Card from "../Card";
 
 export default function Projects (){
     return (
        <div className='projects'>
     
-        
-          <Link to ="/MechanicalDesign" > <h1>Mechancil Projects</h1></Link>
-        
-        
-           <Link to ="/WebPages"> <h1>Web Projects</h1> </Link> 
-        
+        <Card 
+        link="/MechanicalDesign" 
+        name="Mechanical Design" 
+        // image="../images/img-1.jpg"
+        decription="hello this my mech project"
+
+        /> 
     </div>
     
     );
